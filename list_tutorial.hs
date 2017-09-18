99591c15e88464c3ed19040bdaaff90555435d44
FireLemons <shenyang64@gmail.com>
2017-09-18 14:15:48 -0500

    List tutorial progress

--- list_tutorial.hs
+++ list_tutorial.hs
@@ -1,2 +1,14 @@
+import Text.Printf
+
 lostNumbers = [4,8,15,16,23,42]
 
+-- common list functions
+-- use of these on empty list aren't caught at compile time
+main = do if not (null lostNumbers)
+          then do printf "Original List: %s\n" (show lostNumbers)
+                  printf "Head: %s\n" (show (head lostNumbers)) -- first element of list
+                  printf "Tail: %s\n" (show (tail lostNumbers)) -- list without head
+                  printf "Last: %s\n" (show (last lostNumbers)) -- last element
+                  printf "Init: %s\n" (show (init lostNumbers)) -- list without last (Orthogonal to tail)
+                  printf "Length: %s\n" (show (length lostNumbers)) -- length of list
+          else do print "Empty List"
